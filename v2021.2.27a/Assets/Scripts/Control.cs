72039604d47a4e217803f2ba6b96bee8feb5da56
didi didi <Chibouni562@gmail.com>
2021-01-29 22:46:15 +0100

    Desieme forme

---
+++ v2021.2.27a/Assets/Scripts/Control.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class Control : MonoBehaviour {
+	public float speed;
+
+	public float mouseSensibility;
+
+	private PlayerMotor motor;
+
+	void Start () {
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update () {
+		//calcul de la vitesse de mouvment de persnage
+		float xMov = Input.GetAxisRaw ("Horizontal"); 
+		float zMov = Input.GetAxisRaw ("Vertical"); 
+	
+		Vector3 moveHorizontal = transform.right * xMov;
+		Vector3 moveVertical = transform.forward * zMov;
+
+		Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+	
+		motor.move (velocity);
+		//calcul du rotation de personage
+		float yRot = Input.GetAxisRaw ("Mouse X");
+
+		Vector3 rotation = new Vector3 (0, yRot, 0);
+		motor.rotate (rotation);
+		//calcul du rotation de Camera
+		float xRot = Input.GetAxisRaw ("Mouse Y");
+
+		Vector3 CameraRotation = new Vector3 (xRot, 0, 0);
+		//motor.rotateCamera (CameraRotation);
+	}
+}
---
+++ v2021.2.27a/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour 
+{
+	private Vector3 velocity;
+	private Rigidbody rb;
+	private Vector3 rotation;
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	public void move(Vector3 _velocity) 
+	{
+		velocity = _velocity;
+	}
+	public void rotate(Vector3 _rotation) 
+	{
+		rotation = _rotation;
+	}
+
+	private void FixedUpdate()
+	{
+		PerformMovement();
+		PerformRotation ();
+	}
+
+	private void PerformMovement() 
+	{
+		if(velocity != Vector3.zero) 
+		{
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	private void PerformRotation ()
+	{
+		rb.MoveRotation (rb.rotation * Quaternion.Euler(rotation));
+	}
+}
